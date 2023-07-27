0f01212c6c5f7b90b55e2313b533fc7ffda46261
mohamed0690 <mohamedmahrous069@gmail.com>
2023-07-27 14:41:47 +0300

    update icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Catagories from "./components/Catagories/Catagories";
-import Delivery from "./components/Delivery/Delivery";
-import Featured from "./components/Featured/Featured";
-import Footer from "./components/Footer/Footer";
-import Meal from "./components/Meal/Meal";
+import Catagories from "/components/Catagories/Catagories";
+import Delivery from "/components/Delivery/Delivery";
+import Featured from "/components/Featured/Featured";
+import Footer from "/components/Footer/Footer";
+import Meal from "/components/Meal/Meal";
 import NewLetters from "./components/NewLetters/NewLetters";
-import TopNav from "./components/TopNav/TopNav";
-import TopPicks from './components/TopPicks/TopPicks';
+import TopNav from "/components/TopNav/TopNav";
+import TopPicks from '/components/TopPicks/TopPicks';
 
 function App() {
   return (
